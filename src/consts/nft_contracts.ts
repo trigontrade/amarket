63bf7151d4517ce07c7be0ca696763306410d408
The Wizard of Hahz <info@luvnft.com>
2024-06-10 10:35:14 -0400

    updated

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -24,18 +24,19 @@ export const NFT_CONTRACTS: NftContract[] = [
     address: "0x6b869a0cF84147f05a447636c42b8E53De65714E",
     chain: avalancheFuji,
     title: "Steakhouse: Liberatorz",
+    description: "Purchase this Steakhouse: Liberatorz Dmerch for a free steak meal IRL at participating Atlanta restaurants in the ATL5D Discord #foodie channel",
     thumbnailUrl:
       "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeigonh3hde5suwcb3qvkh6ljtvxv7ubfmcqbwfvi3ihoi3igd27jwe/SteakhouseLogo.svg",
     type: "ERC721",
   },
   {
-    address: "0xFfad5A63E1Ed90DaC920A11340f9DfBc0ed3d3ee",
-    chain: baseSepolia,
-    title: "Welcome to ATL5D",
-    description: "You can flip this Welcome to ATL5D sign in the ATL5D Discord.",
+    address: "0xC5A2c72c581eA4A17e17bEeF38a9597132830401",
+    chain: avalancheFuji,
+    title: "Ties",
+    description: "Purchase these Dmerch ties to unlock a free $100 tie.",
     thumbnailUrl:
-      "https://d391b93f5f62d9c15f67142e43841acc.ipfscdn.io/ipfs/bafybeid5v6etpjup37xxann3xhkqaxkoiebku5tqt5xbegnfqvdmvpykji/0.jpg",
-    slug: "atl5d",
+      "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeidaadqapi7twzd7pjp24tu4ngsr3teubrhop7hg3jk3oj6lqysfgm/OS-LOGO.png",
+    slug: "ugly-waifu",
     type: "ERC721",
   },
 
@@ -53,6 +54,7 @@ export const NFT_CONTRACTS: NftContract[] = [
     address: "0x0ACaCa3d3F64bb6e6D3564BBc891c58Bd4A4c83c",
     chain: avalancheFuji,
     title: "GoroBot",
+    description: "Buy this Bot Dmerch to unlock AI ATL5D services.",
     thumbnailUrl:
       "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeiay3ffxy3os56bvnu5cmq7gids4v6n4hf5nvvcb3gy2dzavi3ltnu/profile.jpg",
     slug: "gorobot",
@@ -61,7 +63,8 @@ export const NFT_CONTRACTS: NftContract[] = [
   {
     address: "0x4b6CDEFF5885A57678261bb95250aC43aD490752",
     chain: polygonAmoy,
-    title: "Mata NFT",
+    title: "PPV",
+    description: "Buy this PPV Dmerch to unlock ATL5D Discord PPV events.",
     thumbnailUrl:
       "https://258c828e8cc853bf5e0efd001055fb39.ipfscdn.io/ipfs/bafybeidec7x6bptqmrxgptaedd7wfwxbsccqfogzwfsd4a7duxn4sdmnxy/0.png",
     type: "ERC721",
