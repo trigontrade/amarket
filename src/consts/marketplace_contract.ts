9c666e02e802abc7641b187c474fd5a0b2d9bced
trigontrade <149458283+trigontrade@users.noreply.github.com>
2024-06-13 07:02:12 +0100

    Update marketplace_contract.ts

--- src/consts/marketplace_contract.ts
+++ src/consts/marketplace_contract.ts
@@ -1,6 +1,5 @@
 import type { Chain } from "thirdweb";
-import { avalancheFuji, base, polygonAmoy, sepolia, ethereum } from "./chains";
-import { polygon } from "thirdweb/chains"
+import { bitrock, ethereum } from "./chains";
 
 type MarketplaceContract = {
   address: string;
@@ -13,28 +12,8 @@ type MarketplaceContract = {
  */
 export const MARKETPLACE_CONTRACTS: MarketplaceContract[] = [
   {
-    address: "0x8C1D464B385A2B7EAa80dcAAD66DD8BC0256e717",
-    chain: avalancheFuji,
-  },
-  {
-    address: "0x571B773F1e4A7C080b51C36f37e06f371C515569",
-    chain: polygonAmoy,
-  },
-  {
-    address: "0xe0eFD6fb388405b67b3E9FaFc02649c70E749f03",
-    chain: sepolia,
-  },
-  {
-    address: "0xcc85af4E1EFB3F8A378D20016020124917206E4b",
-    chain: base,
-  },
-  {
-    address: "0xcc85af4E1EFB3F8A378D20016020124917206E4b",
-    chain: sepolia,
-  },
-  {
-    address: "0xaA4F87a8A991EbAeCA3a2d22eF16898D61A2371a",
-    chain: polygon,
+    address: "0xE96952f138850Cd67a61Ac8D97AdB58b3119E157",
+    chain: bitrock,
   },
   {
     address: "0xcc85af4E1EFB3F8A378D20016020124917206E4b",
